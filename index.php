6e2ae15292164ae224f442af065c104ae441c0c3
Alexander <aleksandrs.goncaruks@gmail.com>
2017-08-26 13:32:31 +0300

    add bet history display

--- index.php
+++ index.php
@@ -157,7 +157,7 @@ $lots = [
                   </div>
                   <div class="lot__info">
                       <span class="lot__category"<?=$lot['category'];?></span>
-                      <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lot['title'];?></a></h3>
+                      <h3 class="lot__title"><a class="text-link" href="lot.php"><?=$lot['title'];?></a></h3>
                       <div class="lot__state">
                           <div class="lot__rate">
                               <span class="lot__amount">Стартовая цена</span>
