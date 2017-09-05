e13f28420d13db3ad2841e9928f63a007b7219d1
Alexander <aleksandrs.goncaruks@gmail.com>
2017-09-05 23:20:20 +0300

    feat(lot): add new lot page, add templates for lot and add lot pages

--- lotdata.php
+++ lotdata.php
@@ -8,7 +8,8 @@ $lots = [
     'cost' => '10999',
     'image' => 'img/lot-1.jpg',
     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida tempor augue, sed laoreet velit gravida nec. Fusce nibh lorem, tempor nec diam non, sollicitudin tincidunt neque. Mauris et purus viverra, accumsan dolor vel, cursus dolor. Quisque sollicitudin, leo non ullamcorper blandit, magna magna mollis eros, nec sollicitudin lacus dui.',
-    'min_bet' => '10000'
+    'min_bet' => '10000',
+    'end_date' => '09.09.2017'
   ],
   [
     'title' => 'DC Ply Mens 2016/2017 Snowboard',
@@ -16,7 +17,8 @@ $lots = [
     'cost' => '159999',
     'image' => 'img/lot-2.jpg',
     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida tempor augue, sed laoreet velit gravida nec. Fusce nibh lorem, tempor nec diam non, sollicitudin tincidunt neque. Mauris et purus viverra, accumsan dolor vel, cursus dolor. Quisque sollicitudin, leo non ullamcorper blandit, magna magna mollis eros, nec sollicitudin lacus dui.',
-    'min_bet' => '140000'
+    'min_bet' => '140000',
+    'end_date' => '09.09.2017'
   ],
   [
     'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
@@ -24,7 +26,8 @@ $lots = [
     'cost' => '8000',
     'image' => 'img/lot-3.jpg',
     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida tempor augue, sed laoreet velit gravida nec. Fusce nibh lorem, tempor nec diam non, sollicitudin tincidunt neque. Mauris et purus viverra, accumsan dolor vel, cursus dolor. Quisque sollicitudin, leo non ullamcorper blandit, magna magna mollis eros, nec sollicitudin lacus dui.',
-    'min_bet' => '6000'
+    'min_bet' => '6000',
+    'end_date' => '09.09.2017'
   ],
   [
     'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
@@ -32,7 +35,8 @@ $lots = [
     'cost' => '10999',
     'image' => 'img/lot-4.jpg',
     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida tempor augue, sed laoreet velit gravida nec. Fusce nibh lorem, tempor nec diam non, sollicitudin tincidunt neque. Mauris et purus viverra, accumsan dolor vel, cursus dolor. Quisque sollicitudin, leo non ullamcorper blandit, magna magna mollis eros, nec sollicitudin lacus dui.',
-    'min_bet' => '9000'
+    'min_bet' => '9000',
+    'end_date' => '09.09.2017'
   ],
   [
     'title' => 'Куртка для сноуборда DC Mutiny Charocal',
@@ -40,7 +44,8 @@ $lots = [
     'cost' => '7500',
     'image' => 'img/lot-5.jpg',
     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida tempor augue, sed laoreet velit gravida nec. Fusce nibh lorem, tempor nec diam non, sollicitudin tincidunt neque. Mauris et purus viverra, accumsan dolor vel, cursus dolor. Quisque sollicitudin, leo non ullamcorper blandit, magna magna mollis eros, nec sollicitudin lacus dui.',
-    'min_bet' => '6000'
+    'min_bet' => '6000',
+    'end_date' => '09.09.2017'
   ],
   [
     'title' => 'Маска Oakley Canopy',
@@ -48,6 +53,7 @@ $lots = [
     'cost' => '5400',
     'image' => 'img/lot-6.jpg',
     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida tempor augue, sed laoreet velit gravida nec. Fusce nibh lorem, tempor nec diam non, sollicitudin tincidunt neque. Mauris et purus viverra, accumsan dolor vel, cursus dolor. Quisque sollicitudin, leo non ullamcorper blandit, magna magna mollis eros, nec sollicitudin lacus dui.',
-    'min_bet' => '5000'
+    'min_bet' => '5000',
+    'end_date' => '09.09.2017'
   ]
 ];
